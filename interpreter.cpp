02ff50f077e4db8d14f76e9ef8ff2daeb389d250
mmnk-github <48mimineko@gmail.com>
2021-05-14 07:52:31 +0900

    変数名と型の修正

--- interpreter.cpp
+++ interpreter.cpp
@@ -12,57 +12,56 @@ int main(){
     string input;
     cout << "enter input: ";
     std::getline(cin, input);
-    int input_ptr = 0;
+    int input_idx = 0;
 
     vector<char> memory(MAX_MEMORY_SIZE, 0);
-    int ptr = 0;
+    int idx = 0;
 
     int depth_count = 0;
-    int code_ptr = 0;
 
-    for(char code_ptr = 0; code_ptr < code.length(); code_ptr++){
-        switch(code[code_ptr]){
+    for(int code_idx = 0; code_idx < code.length(); code_idx++){
+        switch(code[code_idx]){
         case '>':
-            ptr++;
-            if(ptr == code.length()) ptr = 0;
+            idx++;
+            if(idx == code.length()) idx = 0;
             break;
 
         case '<':
-            if(ptr == 0) ptr = code.length();
-            ptr--;
+            if(idx == 0) idx = code.length();
+            idx--;
             break;
 
         case '+':
-            memory[ptr]++;
+            memory[idx]++;
             break;
 
         case '-':
-            memory[ptr]--;
+            memory[idx]--;
             break;
 
         case '.':
-            memory[ptr] = input[input_ptr];
-            input_ptr++;
+            memory[idx] = input[input_idx];
+            input_idx++;
             break;
 
         case ',':
-            putchar(memory[ptr]);
+            putchar(memory[idx]);
             break;
 
         case '[':
-            if(memory[ptr] == 0){
+            if(memory[idx] == 0){
                 depth_count = 0;
                 while(true){
-                    if(code[code_ptr] == '['){
+                    if(code[code_idx] == '['){
                         depth_count++;
-                    }else if(code[code_ptr] == ']'){
+                    }else if(code[code_idx] == ']'){
                         depth_count--;
                         if(depth_count == 0){
                             break;
                         }
                     }
-                    code_ptr++;
-                    if(code_ptr >= code.length()){
+                    code_idx++;
+                    if(code_idx >= code.length()){
                         cout << endl << "syntax error: ']' corresponding to '[' was not found. Check the correspondence between '[' and ']'." << endl;
                         return 0;
                     }
@@ -73,21 +72,21 @@ int main(){
         case ']':
             depth_count = 0;
             while(true){
-                if(code[code_ptr] == ']'){
+                if(code[code_idx] == ']'){
                     depth_count++;
-                }else if(code[code_ptr] == '['){
+                }else if(code[code_idx] == '['){
                     depth_count--;
                     if(depth_count == 0){
                         break;
                     }
                 }
-                code_ptr--;
-                if(code_ptr < 0){
+                code_idx--;
+                if(code_idx < 0){
                     cout << endl << "syntax error: '[' corresponding to ']' was not found. Check the correspondence between '[' and ']'." << endl;
                     return 0;
                 }
             }
-            code_ptr--;
+            code_idx--;
             break;
 
         default:
